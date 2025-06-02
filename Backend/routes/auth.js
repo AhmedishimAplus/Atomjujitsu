dd9dcf1f867c0b0ad36805f5df1839823cb3b7fe
AhmedishimA+ <atamerfawzy@gmail.com>
2025-06-02 19:56:05 +0300

    removed admin useless

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
-const Admin = require('../models/Admin');
+
 const auth = require('../middleware/auth');
 
 const router = express.Router();
